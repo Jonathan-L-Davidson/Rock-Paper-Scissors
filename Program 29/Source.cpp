93ea3f4dff9ff28ef81cddc35cafa3572cf2e5b5
Jonathan Davidson <d016527j@student.staffs.ac.uk>
2019-11-20 22:54:13 +0000

    Cleaned up files.
    Added summaries and comments and annotated the code better.

--- Program 29/Source.cpp
+++ Program 29/Source.cpp
@@ -1,17 +1,36 @@
+// Made by Jonathan Davidson.
+
+// -----------------------------------------
+//              Includes
+// -----------------------------------------
 #include <iostream>
 #include <string>
 #include <locale>
 
-// I'm really moving out of my comfort zone here and using something I call my kryptonite, enums!
-enum choice{ROCK = 1, PAPER, SCISSORS, QUIT, ERROR};
-enum matchResult{WIN = 1, LOSS, DRAW};
-bool gameRunning = 1;
+// -----------------------------------------
+//              Definitions
+// -----------------------------------------
+enum choice{ROCK = 1, PAPER, SCISSORS, QUIT, ERROR}; // The choice the player/computer has made.
+enum matchResult{WIN = 1, LOSS, DRAW}; // Result from whether or not the player has won the round or not.
+bool gameRunning = 1; // The game loop.
 
-choice GetSelection(std::string _inputString);
-matchResult CheckForWin(choice _playerInput, choice _computerChoice);
-std::string GetChoice(choice _choice);
+// -----------------------------------------
+//              Prototypes
+// -----------------------------------------
+/// <summary>Returns the choice the player has made via the string.</summary>
+choice GetSelection(std::string &_inputString);
 
+/// <summary>Checks for win/loss/draw states and returns the result.</summary>
+matchResult CheckForWin(choice &_playerInput, choice &_computerChoice);
+
+/// <summary>Returns the string of the choice inputted.</summary>
+std::string GetChoice(choice &_choice);
+
+// -----------------------------------------
+//                Functions
+// -----------------------------------------
 int main() {
+	// Define & Initialise some values.
 	choice playerInput;
 	
 	int playerScore = 0;
@@ -27,6 +46,7 @@ int main() {
 	while(gameRunning) {
 		std::cout << "\n*******************";
 
+		// Check if the game has been won/lost before continuing the loop.
 		if (playerScore >= totalScore) {
 			std::cout << "\nCongratulations! You won!\n";
 			gameRunning = false;
@@ -41,13 +61,15 @@ int main() {
 		std::cout << "\nEnter your selection (Rock, Paper, Scissors): ";
 		std::cin >> strplayerInput;
 		
-		playerInput = GetSelection(strplayerInput);
+		playerInput = GetSelection(strplayerInput); // Retrieves the choice the player has made and stores it into a variable for use later.
 		
+		// Prevent the player from continuing if there's an error. Start over.
 		if (playerInput == ERROR) {
 			std::cout << "\nInvalid input, please enter either 'Rock', 'Paper' or 'Scissors'\n";
 			continue;
 		}
 		
+		// Quit if the player chosen "Quit".
 		if (playerInput == QUIT) {
 			std::cout << "\nExiting...";
 			gameRunning = false;
@@ -59,8 +81,8 @@ int main() {
 
 		std::cout << "You chose: " << GetChoice(playerInput) << "\nThe computer chose: " << GetChoice(computerChoice) << std::endl;
 
-		int matchResult = CheckForWin(playerInput, computerChoice);
-
+		// Determine if the player has won the round or not.
+		int matchResult = CheckForWin(playerInput, computerChoice); 
 		switch (matchResult) {
 			case WIN:
 				std::cout << "\nCongratulations, you win this round!\n";
@@ -78,6 +100,7 @@ int main() {
 				break;
 		}
 
+		// Display the scores.
 		std::cout << "\nPlayer's score is: " << playerScore << " The player needs " << totalScore - playerScore << " points to win.";
 		std::cout << "\nComputer's score is: " << computerScore << " The computer needs " << totalScore - computerScore << " points to win.";
 
@@ -87,10 +110,12 @@ int main() {
 	return 0;
 }
 
-choice GetSelection(std::string _inputString) {
+choice GetSelection(std::string &_inputString) {
+	// Converts string to all lowercase. Useful for players who cannot capitalise their choice.
 	std::locale loc;
 	for (std::string::size_type i = 0; i < _inputString.length(); ++i)	_inputString[i] = std::tolower(_inputString[i], loc);
-
+	
+	// Decides what choice the player has made.
 	if (_inputString == "rock")	return ROCK;
 	else if (_inputString == "paper")	return PAPER;
 	else if (_inputString == "scissors")	return SCISSORS;
@@ -98,7 +123,7 @@ choice GetSelection(std::string _inputString) {
 	else return ERROR;
 }
 
-matchResult CheckForWin(choice _playerInput, choice _computerChoice) {
+matchResult CheckForWin(choice &_playerInput, choice &_computerChoice) {
 	// Draw!
 	if (_computerChoice == _playerInput)	return DRAW;
 	
@@ -107,7 +132,7 @@ matchResult CheckForWin(choice _playerInput, choice _computerChoice) {
 	else return LOSS; // All possible win conditions exhausted, call loss.
 }
 
-std::string GetChoice(choice _choice) {
+std::string GetChoice(choice &_choice) {
 	switch (_choice) {
 	case ROCK:
 		return "Rock";
